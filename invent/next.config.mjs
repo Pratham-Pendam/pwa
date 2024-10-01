3ad60f3db89e695eb9a94ae4f917e408ed43bf03
Pratham-Pendam <83838635+Pratham-Pendam@users.noreply.github.com>
2024-10-01 11:59:12 +0530

    Update next.config.mjs

--- invent/next.config.mjs
+++ invent/next.config.mjs
@@ -1,9 +1,24 @@
+/** @type {import('next').NextConfig} */
 import withPWAInit from "@ducanh2912/next-pwa";
 
+// Initialize the PWA plugin
 const withPWA = withPWAInit({
   dest: "public",
 });
 
-export default withPWA({
-    reactStrictMode: true,
-});
+const nextConfig = {
+  reactStrictMode: true, // Enable React Strict Mode
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://s4-inventorymanagement.s3.ap-south-1.amazonaws.com/", // Allow images from this hostname
+        port: "",
+        pathname: "/**", // Match any path for images
+      },
+    ],
+  },
+};
+
+// Export the combined config with PWA support
+export default withPWA(nextConfig);
