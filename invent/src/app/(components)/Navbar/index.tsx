0d25adcf587eb7b95771c68bb15d3833c49f82b8
Pratham-Pendam <83838635+Pratham-Pendam@users.noreply.github.com>
2024-11-02 04:56:21 +0530

    Update index.tsx

--- invent/src/app/(components)/Navbar/index.tsx
+++ invent/src/app/(components)/Navbar/index.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://s4-inventorymanagement.s3.ap-south-1.amazonaws.com/profile.jpg" */}"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" 
               alt="Profile"
               width={100}
               height={50}
